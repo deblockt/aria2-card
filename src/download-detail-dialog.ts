ff143d720649cf581971a7385ab71418413f27f2
Maxym Ocheretianko <bearpawmaxim@users.noreply.github.com>
2022-08-04 21:53:18 +0300

    feat:  download detail popin adjust
    - change download size formatting
    - Fix infinity string
    - Adjust time format
    - Add progress percent to download detail card

--- src/download-detail-dialog.ts
+++ src/download-detail-dialog.ts
@@ -1,11 +1,9 @@
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import { Download, remainingDurationInSecond } from './download';
-
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { Download, remainingDurationInSecond, downloadedPercent } from './download';
 
 @customElement('download-detail-dialog')
 export class DownloadDetailDialog extends LitElement {
-
   static override styles = css`
     .label {
       color: var(--secondary-text-color);
@@ -50,8 +48,9 @@ export class DownloadDetailDialog extends LitElement {
     let downloadData = html``
     if (this.currentDownload.status !== 'complete' && this.currentDownload.status !== 'paused') {
       downloadData = html`
-        <span class="label"> download speed: </span> <span class="value"> ${this.formatSize(this.currentDownload.download_speed)}/s </span> <br/>
-        <span class="label"> remaining time: </span> <span class="value"> ${this.buildRemainingTime(this.currentDownload)} </span> <br/>
+        <span class="label">download speed: </span> <span class="value"> ${this.formatSize(this.currentDownload.download_speed)}/s </span> <br/>
+        <span class="label">progress: </span> <span class="value"> ${this.buildProgress(this.currentDownload)} </span> <br/>
+        <span class="label">remaining time: </span> <span class="value"> ${this.buildRemainingTime(this.currentDownload)} </span> <br/>
       `;
     }
 
@@ -82,8 +81,20 @@ export class DownloadDetailDialog extends LitElement {
     }
   }
 
+  buildProgress(download: Download) {
+    const downloadPercent = downloadedPercent(download);
+    return downloadPercent.toFixed(2)
+      + '% (' + this.formatSize(download.completed_length)
+      + ' of ' + this.formatSize(download.total_length)
+      + ')';
+  }
+
   buildRemainingTime(download: Download) {
     const remaingTimeInSecond = remainingDurationInSecond(download);
+
+    if (!isFinite(remaingTimeInSecond)) {
+      return 'infinity';
+    }
 
     const hours = Math.floor(remaingTimeInSecond / 3600);
     const minutes = Math.floor((remaingTimeInSecond - (hours * 3600)) / 60);
@@ -91,34 +102,40 @@ export class DownloadDetailDialog extends LitElement {
 
     let result = '';
     if (hours > 0) {
-      result += hours + 'h '
+      result += hours + ' h '
     }
     if (minutes > 0) {
-      result += minutes + 'm '
+      result += minutes + ' m '
     }
     if (hours == 0) {
-      result += seconds + 's'
+      result += seconds + ' s'
     }
 
     return result;
   }
 
-  formatSize(byte: number) {
-    const gigaBytes = byte / 1_073_741_824;
-    if (gigaBytes >= 1) {
-      return (Math.round(gigaBytes * 100) / 100) + 'Go';
-    }
-    const megaBytes = byte / 1_048_576;
-    if (megaBytes >= 1) {
-      return (Math.round(megaBytes * 100) / 100) + 'Mo';
+  formatSize(size: number) {
+    const sizeUnits = [ 'B', 'KB', 'MB', 'GB' ];
+    let unit = sizeUnits[0];
+
+    if (!size) {
+      size = 0;
     }
 
-    return byte + ' o';
+    for (var i = 1; i < sizeUnits.length; i++) {
+      if (size >= 1024) {
+        size = size / 1024;
+        unit = sizeUnits[i];
+      } else {
+        break;
+      }
+    }
+    return size.toFixed(2) + ' ' + unit;
   }
 
   closeDetail() {
     if (window.history.state && window.history.state.currentDownload === this.currentDownload?.gid) {
-      window.history.back()
+      window.history.back();
     }
     this.currentDownload = undefined;
   }
@@ -126,7 +143,7 @@ export class DownloadDetailDialog extends LitElement {
   override connectedCallback() {
     super.connectedCallback();
     window.addEventListener("popstate", () => {
-      this.closeDetail()
+      this.closeDetail();
     })
   }
 }
