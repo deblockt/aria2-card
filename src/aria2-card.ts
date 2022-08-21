7c6a7cfb70bb146e94f015435b27f8b50a07ada8
Thomas Deblock <deblock.thomas.62@gmail.com>
2022-08-21 09:47:29 +0200

    fix: card startup issue when not used on main panel

--- src/aria2-card.ts
+++ src/aria2-card.ts
@@ -176,13 +176,9 @@ export class Aria2Card extends LitElement {
     this.config = config
   }
 
-  override connectedCallback() {
-    super.connectedCallback();
-
-    if (this.hass) {
-      this.hass!.connection.subscribeEvents((e: any) => this._refresh(e.data.list), 'download_list_updated')
-      this.hass!.callService('aria2', 'refresh_downloads')
-    }
+  override firstUpdated() {
+    this.hass!.connection.subscribeEvents((e: any) => this._refresh(e.data.list), 'download_list_updated');
+    this.hass!.callService('aria2', 'refresh_downloads');
   }
 
   getCardSize() {
