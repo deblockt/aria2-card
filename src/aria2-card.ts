3a189a23a827560dc60ddb8eba436cd22c59a927
Thomas Deblock <deblock.thomas.62@gmail.com>
2025-10-13 21:38:11 +0200

    feat: add upload torrent button (#16)

--- src/aria2-card.ts
+++ src/aria2-card.ts
@@ -1,9 +1,9 @@
 import {LitElement, html, css} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
 import {repeat} from 'lit/directives/repeat.js';
-import { Download, downloadedPercent } from './download';
-import { DownloadDetailDialog } from './download-detail-dialog';
-import { localize } from './localize/localize';
+import {Download, downloadedPercent} from './download';
+import {DownloadDetailDialog} from './download-detail-dialog';
+import {localize} from './localize/localize';
 
 @customElement('aria2-card')
 export class Aria2Card extends LitElement {
@@ -12,6 +12,10 @@ export class Aria2Card extends LitElement {
       display: flex;
       flex-direction: row;
       align-items: center;
+    }
+
+    .trailing {
+      display: flex;
     }
 
     .start-download-row > .addBox {
@@ -27,7 +31,7 @@ export class Aria2Card extends LitElement {
       display: flex;
       flex-direction: row;
       align-items: center;
-      position: relative
+      position: relative;
     }
 
     .progress {
@@ -57,79 +61,151 @@ export class Aria2Card extends LitElement {
 
   override render() {
     if (!this.config || !this.hass) {
-      return html``
+      return html``;
     }
     if (!('entry_id' in this.config)) {
-      return html`<ha-card> <div class="card-content"> You should edit the card to select the aria server to use. </div> </ha-card>`
+      return html`<ha-card>
+        <div class="card-content">
+          You should edit the card to select the aria server to use.
+        </div>
+      </ha-card>`;
     }
 
     return html`
       <ha-card>
         <div class="card-content">
           <div class="start-download-row">
-            <ha-textfield icon class="addBox" iconTrailing="true" placeholder="${localize('card.url_to_download')}" @keydown=${this.startDownloadOnEnter}>
+            <ha-textfield
+              icon
+              class="addBox"
+              iconTrailing="true"
+              placeholder="${localize('card.url_to_download')}"
+              @keydown=${this.startDownloadOnEnter}
+            >
               <div class="trailing" slot="trailingIcon">
-                <ha-icon-button @click="${this.startDownload}">
+                <ha-icon-button @click="${this.startDownload}" title="start to download">
                   <ha-icon style="display: flex" icon="hass:play"></ha-icon>
                 </ha-icon-button>
+                <ha-icon-button
+                  title="Upload torrent file"
+                  @click="${() => this.shadowRoot?.getElementById('fileInput')?.click()}"
+                >
+                  <ha-icon style="display: flex" icon="hass:upload"></ha-icon>
+                </ha-icon-button>
+                <input
+                  type="file"
+                  id="fileInput"
+                  accept=".torrent"
+                  @change="${this.uploadFile}"
+                  style="display: none;"
+                />
               </div>
-            </ha-textfield> 
+            </ha-textfield>
           </div>
           <div class="download-list">
-            ${
-              repeat(
-                (this.downloads || []).slice(0, 10),
-                (downwload: Download) => downwload.gid,
-                (download: Download) => html`
-              <div class="item">
-                ${this.buildProgressBar(download)}
-                <div class="info">
-                  <span @click="${() => this.openDetail(download)}" class="name">${download.name}</span>
-                  ${this.buildDownloadIcon(download)}
+            ${repeat(
+              (this.downloads || []).slice(0, 10),
+              (downwload: Download) => downwload.gid,
+              (download: Download) => html`
+                <div class="item">
+                  ${this.buildProgressBar(download)}
+                  <div class="info">
+                    <span
+                      @click="${() => this.openDetail(download)}"
+                      class="name"
+                      >${download.name}</span
+                    >
+                    ${this.buildDownloadIcon(download)}
+                  </div>
                 </div>
-              </div>
-            `)}
+              `
+            )}
           </div>
         </div>
       </ha-card>
     `;
   }
 
-  openDetail(download: Download) {
-    const hassShadowRoot = document.getElementsByTagName('home-assistant')[0].shadowRoot
+  async uploadFile(event: InputEvent & { target: HTMLInputElement }) {
+    const readFile = (file: File): Promise<string> => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+      });
+    };
 
-    const existingDownloadingDialog: DownloadDetailDialog | null | undefined = hassShadowRoot?.querySelector('download-detail-dialog')
+    const files = event.target.files as FileList;
+    if (files.length > 0) {
+      const fileContent = (await readFile(files[0])).split(',')[1];
+      await this.hass.callService('aria2', 'start_download', {
+        url: fileContent,
+        server_entry_id: this.entryId,
+      });
+    }
+  }
+
+  openDetail(download: Download) {
+    const hassShadowRoot =
+      document.getElementsByTagName('home-assistant')[0].shadowRoot;
+
+    const existingDownloadingDialog: DownloadDetailDialog | null | undefined =
+      hassShadowRoot?.querySelector('download-detail-dialog');
 
     if (existingDownloadingDialog) {
-      existingDownloadingDialog.currentDownload = download
+      existingDownloadingDialog.currentDownload = download;
     } else {
-      const newDialog: any = document.createElement('download-detail-dialog')
-      newDialog.currentDownload = download
-      hassShadowRoot?.appendChild(newDialog)
+      const newDialog: any = document.createElement('download-detail-dialog');
+      newDialog.currentDownload = download;
+      hassShadowRoot?.appendChild(newDialog);
     }
   }
 
   buildDownloadIcon(download: Download) {
     if (download.status === 'complete' || download.status === 'removed') {
-      return html`<ha-icon-button disabled="disabled"><ha-icon style="display: flex" icon="hass:check"></ha-icon></ha-icon-button>`;
+      return html`<ha-icon-button disabled="disabled"
+        ><ha-icon style="display: flex" icon="hass:check"></ha-icon
+      ></ha-icon-button>`;
     } else if (download.status === 'paused') {
       return html`
-      <ha-icon-button @click="${() => this.actionOnDownload('resume', download)}"><ha-icon style="display: flex" icon="hass:play"></ha-icon></ha-icon-button>
-      <ha-icon-button @click="${() => this.actionOnDownload('remove', download)}"><ha-icon style="display: flex" icon="hass:stop" ></ha-icon></ha-icon-button>
+        <ha-icon-button
+          @click="${() => this.actionOnDownload('resume', download)}"
+          ><ha-icon style="display: flex" icon="hass:play"></ha-icon
+        ></ha-icon-button>
+        <ha-icon-button
+          @click="${() => this.actionOnDownload('remove', download)}"
+          ><ha-icon style="display: flex" icon="hass:stop"></ha-icon
+        ></ha-icon-button>
       `;
     } else {
       return html`
-      <ha-icon-button @click="${() => this.actionOnDownload('pause', download)}"><ha-icon style="display: flex" icon="hass:pause"></ha-icon></ha-icon-button>
-      <ha-icon-button @click="${() => this.actionOnDownload('remove', download)}"><ha-icon style="display: flex" icon="hass:stop" ></ha-icon></ha-icon-button>
+        <ha-icon-button
+          @click="${() => this.actionOnDownload('pause', download)}"
+          ><ha-icon style="display: flex" icon="hass:pause"></ha-icon
+        ></ha-icon-button>
+        <ha-icon-button
+          @click="${() => this.actionOnDownload('remove', download)}"
+          ><ha-icon style="display: flex" icon="hass:stop"></ha-icon
+        ></ha-icon-button>
       `;
     }
   }
 
   buildProgressBar(download: Download) {
-    if ((download.status !== 'complete' && download.status !== 'removed') && (!download.is_torrent || !download.seeder)) {
+    if (
+      download.status !== 'complete' &&
+      download.status !== 'removed' &&
+      (!download.is_torrent || !download.seeder)
+    ) {
       return html`
-        <div class="progress" style="width: ${downloadedPercent(download, download.completed_length)}%">
-        </div>
+        <div
+          class="progress"
+          style="width: ${downloadedPercent(
+            download,
+            download.completed_length
+          )}%"
+        ></div>
       `;
     } else {
       return html``;
@@ -137,38 +213,59 @@ export class Aria2Card extends LitElement {
   }
 
   get _downloadItem(): any | undefined {
-    return this.shadowRoot?.querySelector(".addBox");
+    return this.shadowRoot?.querySelector('.addBox');
   }
 
   async startDownloadOnEnter(ev: any) {
     if (ev.keyCode === 13) {
-      this.startDownload()
+      this.startDownload();
     }
   }
 
   async startDownload() {
-    if (this._downloadItem && this._downloadItem.value && this._downloadItem.value.length > 0) {
-      await this.hass.callService('aria2', 'start_download', {'url': this._downloadItem.value, 'server_entry_id': this.entryId})
-      this._downloadItem.value = ''
+    if (
+      this._downloadItem &&
+      this._downloadItem.value &&
+      this._downloadItem.value.length > 0
+    ) {
+      await this.hass.callService('aria2', 'start_download', {
+        url: this._downloadItem.value,
+        server_entry_id: this.entryId,
+      });
+      this._downloadItem.value = '';
     }
   }
 
-  async actionOnDownload(action: 'pause' | 'remove' | 'resume', download: Download) {
-    await this.hass.callService('aria2', action + '_download', {'gid': download.gid, 'server_entry_id': this.entryId})
+  async actionOnDownload(
+    action: 'pause' | 'remove' | 'resume',
+    download: Download
+  ) {
+    await this.hass.callService('aria2', action + '_download', {
+      gid: download.gid,
+      server_entry_id: this.entryId,
+    });
   }
 
-  async _refresh(aria_state: {server_entry_id?: string, list: Download[]}) {
-    if (aria_state.server_entry_id && this.entryId !== aria_state.server_entry_id) {
+  async _refresh(aria_state: {server_entry_id?: string; list: Download[]}) {
+    if (
+      aria_state.server_entry_id &&
+      this.entryId !== aria_state.server_entry_id
+    ) {
       return;
     }
 
-    this.downloads = aria_state.list
+    this.downloads = aria_state.list;
 
-    const hassShadowRoot = document.getElementsByTagName('home-assistant')[0].shadowRoot
-    const existingDownloadingDialog: DownloadDetailDialog | null | undefined = hassShadowRoot?.querySelector('download-detail-dialog')
+    const hassShadowRoot =
+      document.getElementsByTagName('home-assistant')[0].shadowRoot;
+    const existingDownloadingDialog: DownloadDetailDialog | null | undefined =
+      hassShadowRoot?.querySelector('download-detail-dialog');
 
     if (existingDownloadingDialog?.currentDownload) {
-      const refreshedDownload = this.downloads.filter(download => download.gid == existingDownloadingDialog?.currentDownload?.gid);
+      const refreshedDownload = this.downloads.filter(
+        (download) =>
+          download.gid == existingDownloadingDialog?.currentDownload?.gid
+      );
       if (refreshedDownload.length > 0) {
         existingDownloadingDialog.currentDownload = refreshedDownload[0];
       }
@@ -186,16 +283,23 @@ export class Aria2Card extends LitElement {
     }
     if (this.hass) {
       this.downloads = [];
-      this.hass!.callService('aria2', 'refresh_downloads', {'server_entry_id': this.entryId});
+      this.hass!.callService('aria2', 'refresh_downloads', {
+        server_entry_id: this.entryId,
+      });
     }
   }
 
   override firstUpdated() {
-    this.hass!.connection.subscribeEvents((e: any) => this._refresh(e.data), 'download_list_updated');
+    this.hass!.connection.subscribeEvents(
+      (e: any) => this._refresh(e.data),
+      'download_list_updated'
+    );
     if (!this.entryId) {
       return;
     }
-    this.hass!.callService('aria2', 'refresh_downloads', {'server_entry_id': this.entryId});
+    this.hass!.callService('aria2', 'refresh_downloads', {
+      server_entry_id: this.entryId,
+    });
   }
 
   getCardSize() {
@@ -213,6 +317,11 @@ declare global {
   }
 
   interface Window {
-    customCards: {type: string; name: string; preview: boolean; description: string}[];
-   }
+    customCards: {
+      type: string;
+      name: string;
+      preview: boolean;
+      description: string;
+    }[];
+  }
 }
